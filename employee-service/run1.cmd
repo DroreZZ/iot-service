ad3cac045ef1176bde878b8d88313cec47946fa7
ZhiBin <zhibingoo@163.com>
2018-05-24 11:08:15 +0800

    fixed gateway route configure.

---
+++ employee-service/run1.cmd
@@ -0,0 +1,2 @@
+java   -jar target\employee-service-1.0-SNAPSHOT.jar  --spring.profiles.active=instance2
+rem java  -Dserver.port=2223  -jar target\config-service-1.0-SNAPSHOT.jar  --spring.profiles.active=native
